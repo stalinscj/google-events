e51f4c8d3ec31584e0b70614c3b4806ca719863f
stalinscj <ssanchez684@gmail.com>
2022-05-05 06:35:07 -0400

    Prepare users for oauth

---
+++ database/migrations/2022_04_30_212002_alter_users_table_to_allow_oauth.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->nullable()->change();
+            $table->string('email')->nullable()->change();
+            $table->string('password')->nullable()->change();
+            $table->string('google_id')->nullable()->after('password');
+            $table->text('google_token')->nullable()->after('google_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->change();
+            $table->string('email')->unique()->change();
+            $table->string('password')->change();
+            $table->dropColumn('google_id');
+            $table->dropColumn('google_token');
+        });
+    }
+};
